96ac22f597dbd688e4da38d860f82b2994adde4e
Cesar Segura <cesar.segura@ucem.ac.cr>
2025-01-20 21:49:33 -0600

    Envío de correo electrónico funcional

--- src/components/servicios/ServiciosSwiper.tsx
+++ src/components/servicios/ServiciosSwiper.tsx
@@ -1,47 +1,47 @@
 /* eslint-disable @next/next/no-img-element */
-'use client';
+'use client'
 
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay, Navigation } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Autoplay, Navigation } from 'swiper/modules'
 
-import 'swiper/css';
-import 'swiper/css/navigation';
+import 'swiper/css'
+import 'swiper/css/navigation'
 
 interface FormatImageFromAPI {
-  name: string;
-  hash: string;
-  ext: string;
-  mime: string;
-  width: number;
-  height: number;
-  size: number;
-  sizeInBytes: number;
-  url: string;
+  name: string
+  hash: string
+  ext: string
+  mime: string
+  width: number
+  height: number
+  size: number
+  sizeInBytes: number
+  url: string
 }
 interface ImageFromAPI {
-  id: number;
+  id: number
   attributes: {
-    name: string;
-    width: number;
-    height: number;
+    name: string
+    width: number
+    height: number
     formats: {
-      thumbnail: FormatImageFromAPI;
-      large: FormatImageFromAPI;
-      medium: FormatImageFromAPI;
-      small: FormatImageFromAPI;
-    };
-    url: string;
-  };
+      thumbnail: FormatImageFromAPI
+      large: FormatImageFromAPI
+      medium: FormatImageFromAPI
+      small: FormatImageFromAPI
+    }
+    url: string
+  }
 }
 interface Props {
-  images: ImageFromAPI[];
+  images: ImageFromAPI[]
 }
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL ?? 'https://admin.ecokhemia.com';
-const imagePath = (url: string) => `${API_URL}${url}`;
+const API_URL = process.env.NEXT_PUBLIC_API_URL ?? ''
+const imagePath = (url: string) => `${API_URL}${url}`
 
 export default function ServiciosSwiper(props: Props) {
-  const { images } = props;
+  const { images } = props
   return (
     <Swiper
       className='z-0 h-full'
@@ -54,19 +54,17 @@ export default function ServiciosSwiper(props: Props) {
       modules={[Autoplay, Navigation]}
       allowTouchMove={false}
     >
-      {
-        images.map((image, index) => (
-          <SwiperSlide className='relative' key={index}>
-            <img
-              className='object-cover object-center w-full min-h-[500px] xl:min-h-[450px] h-full max-h-[500px] xl:max-h-[450px]'
-              width={1920}
-              height={1080}
-              src={imagePath(image.attributes.url)}
-              alt='Ecokhemia'
-            />
-          </SwiperSlide>
-        ))
-      }
+      {images.map((image, index) => (
+        <SwiperSlide className='relative' key={index}>
+          <img
+            className='object-cover object-center w-full min-h-[500px] xl:min-h-[450px] h-full max-h-[500px] xl:max-h-[450px]'
+            width={1920}
+            height={1080}
+            src={imagePath(image.attributes.url)}
+            alt='Ecokhemia'
+          />
+        </SwiperSlide>
+      ))}
     </Swiper>
-  );
+  )
 }
