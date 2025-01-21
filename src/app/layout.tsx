e5fe241250b6195abce735895bf62fdec08c5566
Cesar Segura <cesar.segura@ucem.ac.cr>
2025-01-20 21:37:38 -0600

    Envío de correo electrónico funcional

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,27 @@
-import type { ChildrenInterface } from '@/interfaces/general';
-import AppProvider from '@/contexts/AppContext';
-import Navbar from '@/components/layout/Navbar';
-import Footer from '@/components/layout/Footer';
+import type { ChildrenInterface } from '@/interfaces/general'
+import AppProvider from '@/contexts/AppContext'
+import Navbar from '@/components/layout/Navbar'
+import Footer from '@/components/layout/Footer'
 
-import { config } from '@fortawesome/fontawesome-svg-core';
-import '@fortawesome/fontawesome-svg-core/styles.css';
-import '@/lib/fontawesome';
-import './globals.css';
-import OfertaLaboral from '@/components/modals/OfertaLaboral';
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+import '@/lib/fontawesome'
+import './globals.css'
+import OfertaLaboral from '@/components/modals/OfertaLaboral'
 
-import { SITE_URL } from '@/constants/general';
+import { SITE_URL } from '@/constants/general'
 
-config.autoAddCss = false;
+config.autoAddCss = false
 
 export const metadata = {
+  metadataBase: new URL('https://ecokhemia.com'),
   title: 'Ecokhemia',
-  description: 'Somos una empresa de limpieza industrial que utiliza diversas tecnologías para ofrecer soluciones eficientes, seguras y responsables con el medio ambiente.',
+  description:
+    'Somos una empresa de limpieza industrial que utiliza diversas tecnologías para ofrecer soluciones eficientes, seguras y responsables con el medio ambiente.',
   openGraph: {
     title: 'Ecokhemia',
-    description: 'Somos una empresa de limpieza industrial que utiliza diversas tecnologías para ofrecer soluciones eficientes, seguras y responsables con el medio ambiente.',
+    description:
+      'Somos una empresa de limpieza industrial que utiliza diversas tecnologías para ofrecer soluciones eficientes, seguras y responsables con el medio ambiente.',
     url: SITE_URL,
     siteName: 'Ecokhemia - Servicios de limpieza industrial',
     images: [
@@ -26,28 +29,29 @@ export const metadata = {
         url: `${SITE_URL}/metadata.png`,
         width: 800,
         height: 600,
-        alt: 'Ecokhemia - Servicios de limpieza industrial',
-      },
+        alt: 'Ecokhemia - Servicios de limpieza industrial'
+      }
     ],
-    type: 'website',
+    type: 'website'
   },
   twitter: {
     card: 'summary_large_image',
     site: '@ecokhemia',
     title: 'Ecokhemia - Servicios de limpieza industrial',
-    description: 'Somos una empresa de limpieza industrial que utiliza diversas tecnologías para ofrecer soluciones eficientes, seguras y responsables con el medio ambiente.',
-    images: [`${SITE_URL}/metadata.png`],
+    description:
+      'Somos una empresa de limpieza industrial que utiliza diversas tecnologías para ofrecer soluciones eficientes, seguras y responsables con el medio ambiente.',
+    images: [`${SITE_URL}/metadata.png`]
   },
   icons: {
     shortcut: '/favicon.ico'
   }
-};
+}
 
 export default function RootLayout({ children }: ChildrenInterface) {
   return (
     <html lang='es'>
       <AppProvider>
-        <body className="flex flex-col justify-between min-h-screen font-sans antialiased bg-[#fdfdfd]">
+        <body className='flex flex-col justify-between min-h-screen font-sans antialiased bg-[#fdfdfd]'>
           <Navbar />
           {children}
           <OfertaLaboral />
@@ -55,5 +59,5 @@ export default function RootLayout({ children }: ChildrenInterface) {
         </body>
       </AppProvider>
     </html>
-  );
+  )
 }
